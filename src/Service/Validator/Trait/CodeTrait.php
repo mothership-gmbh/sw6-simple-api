6854d67bc8d751e166dc9e81dbe2e7f60b535dca
Stefan Schnellberger <schnellberger@mothership.de>
2023-06-07 17:12:44 +0200

    SU-304: CustomField, PropertyGroup & PropertyGroupOption werden validiert und nur noch in camelCase oder snake_case akzeptiert

--- src/Service/Validator/Trait/CodeTrait.php
+++ src/Service/Validator/Trait/CodeTrait.php
@@ -15,7 +15,7 @@ trait CodeTrait
     protected function hasValidFormat(string $code): void
     {
         // Ist Code nicht in camelCase oder snake_case?
-        if (!preg_match('/^([a-z0-9]+)([A-Z]?[a-z0-9]+)+$/', $code) || !preg_match('/^[a-z0-9]+(_[a-z0-9]+)*$/', $code)) {
+        if (!preg_match('/^([a-z0-9]+)([A-Z]?[a-z0-9]+)+$/', $code) && !preg_match('/^[a-z0-9]+(_[a-z0-9]+)*$/', $code)) {
             throw new InvalidCodeFormatException("The format of the provided code [$code] is invalid. Use either camelCase or snake_case. The label or display name can be manually changed later on.");
         }
     }
