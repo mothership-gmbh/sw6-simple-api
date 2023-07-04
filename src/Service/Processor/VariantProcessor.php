edc65508b47d41968d1fe02d6fad270f27ff56d7
Stefan Schnellberger <schnellberger@mothership.de>
2023-07-04 13:53:19 +0200

    VariantenProdukt mit gleichbleibender SKU kann neue Variantenachsen bekommen

--- src/Service/Processor/VariantProcessor.php
+++ src/Service/Processor/VariantProcessor.php
@@ -8,8 +8,10 @@ use MothershipSimpleApi\Service\Definition\Product\Request;
 use MothershipSimpleApi\Service\Exception\ProductNotFoundException;
 use MothershipSimpleApi\Service\Exception\PropertyGroupOptionNotFoundException;
 use MothershipSimpleApi\Service\Helper\BitwiseOperations;
+use Shopware\Core\Content\Product\ProductCollection;
 use Shopware\Core\Content\Product\ProductEntity;
 use Shopware\Core\Content\Property\Aggregate\PropertyGroupOption\PropertyGroupOptionEntity;
+use Shopware\Core\Content\Property\PropertyGroupEntity;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -22,7 +24,9 @@ class VariantProcessor
     public function __construct(
         protected EntityRepository $productRepository,
         protected EntityRepository $productConfiguratorRepository,
-        protected EntityRepository $propertyGroupOptionRepository
+        protected EntityRepository $propertyGroupOptionRepository,
+        protected EntityRepository $propertyGroupRepository,
+        protected EntityRepository $productOptionRepository
     )
     {
     }
@@ -39,7 +43,6 @@ class VariantProcessor
         foreach ($request->getVariants() as $variantProduct) {
             $variantUuid = $this->getProductUuid($variantProduct->getSku(), $context);
             $this->loadEntityById($variantUuid, $context);
-
 
             $this->loadProductConfiguratorSettingsByProductId($parentUuid, $context);
             $dataConfiguratorSettings = [
@@ -80,30 +83,47 @@ class VariantProcessor
 
     protected function determineVariantsToBeDeleted(string $parentUuid, Request $request, Context $context): void
     {
-        $assignedVariants = $this->loadEntityById($parentUuid, $context)->getChildren();
+        $assignedVariants = $this->loadEntityById($parentUuid, $context)?->getChildren();
         $expectedVariants = $request->getVariants();
         $expectedVariantsSku = [];
         foreach ($expectedVariants as $expectedVariant) {
-            $expectedVariantsSku[] = $expectedVariant->getSku();
+            $expectedVariantsSku[$expectedVariant->getSku()] = $expectedVariant->getAxis();
         }
 
+        $assignedVariantsOptions = $this->transformOptionsToSimpleApiFormat($assignedVariants, $context);
+
         foreach ($assignedVariants as $assignedVariant) {
-            if ($assignedVariant->getProductNumber() && !in_array(
-                    $assignedVariant->getProductNumber(),
-                    $expectedVariantsSku,
-                    true
+            $sku = $assignedVariant->getProductNumber();
+            if ($assignedVariant->getProductNumber()
+                    && (!array_key_exists(
+                        $assignedVariant->getProductNumber(),
+                        $expectedVariantsSku,
+                )
                 )) {
                 $this->productRepository->delete([['id' => $assignedVariant->getId()]], $context);
+            }
+            /*
+             * Hat die neue erwartete Variante andere Variantenachsen als die bestehende Variante mit der gleichen SKU,
+             * sollen die bestehenden Variantenachsen gelöscht werden.
+             * Informationen zu den Variantenachsen sind in Shopware in 2 speziellen Tabellen
+             * (product_configurator_setting & product_option) gespeichert.
+             * Hinweis: Wir dürfen hier nicht einfach das Variantenprodukt löschen, da die product-Entität mit
+             * Bestellungen in der order-Tabelle verknüpft ist.
+             */
+            if (isset($assignedVariantsOptions[$sku], $expectedVariantsSku[$sku])
+                && !$this->arraysAreEqual($assignedVariantsOptions[$sku], $expectedVariantsSku[$sku])) {
+                $this->deleteProductConfiguratorSettings($parentUuid, $context);
+                $this->deleteProductOptions($assignedVariant->getId(), $context);
             }
         }
     }
 
-    protected function loadEntityById(string $productUuid, Context $context): ProductEntity
+    protected function loadEntityById(string $productUuid, Context $context): ?ProductEntity
     {
         $criteria = new Criteria();
         $criteria->addAssociations(['configuratorSettings', 'children']);
         $criteria->addFilter(new EqualsFilter('id', $productUuid));
-        return $this->productRepository->search($criteria, $context)->first();
+        return $this->productRepository->search($criteria, $context)?->first();
     }
 
     protected function loadProductConfiguratorSettingsByProductId(string $productUuid, Context $context): array
@@ -180,11 +200,78 @@ class VariantProcessor
         return $this->propertyGroupOptionRepository->search($criteria, $context)->first();
     }
 
+    protected function getPropertyGroupById(string $propertyGroupId, Context $context): PropertyGroupEntity|null
+    {
+        $criteria = new Criteria();
+        $criteria->setIds([strtolower($propertyGroupId)]);
+        return $this->propertyGroupRepository->search($criteria, $context)->first();
+    }
+
     protected function getPropertyGroupOptionByCode(string $propertyGroupOptionCode, Context $context): PropertyGroupOptionEntity|null
     {
         $criteria = new Criteria();
         $criteria->addFilter(new EqualsFilter('customFields.code', $propertyGroupOptionCode));
 
         return $this->propertyGroupOptionRepository->search($criteria, $context)->first();
+    }
+
+    protected function transformOptionsToSimpleApiFormat(ProductCollection|null $assignedVariants, Context $context): array
+    {
+        $transformedVariants = [];
+        $assignedVariantsArr = $assignedVariants?->getElements();
+        foreach ($assignedVariantsArr as $assignedVariant) {
+            $optionIds = $assignedVariant->getOptionIds();
+            $sku = $assignedVariant->getProductNumber();
+            $transformedVariants[$sku] = [];
+            foreach ($optionIds as $optionId) {
+                $option = $this->getPropertyGroupOptionById($optionId, $context);
+                $groupId = $option?->getGroupId();
+                $groupCode = $this->getPropertyGroupById($groupId, $context)?->getCustomFields()['code'];
+                if (!array_key_exists($groupCode, $transformedVariants[$sku])) {
+                    $transformedVariants[$sku][$groupCode] = [];
+                }
+                $optionCode = $option?->getCustomFields()['code'];
+                $transformedVariants[$sku][$groupCode][] = $optionCode;
+            }
+        }
+        return $transformedVariants;
+    }
+
+    protected function arraysAreEqual(array $assigned, array $expected): bool
+    {
+        foreach ($assigned as $groupCode => $value) {
+            sort($assigned[$groupCode]);
+            if (array_key_exists($groupCode, $expected)) {
+                sort($expected[$groupCode]);
+                } else {
+                return false;
+            }
+        }
+
+        return $assigned === $expected;
+    }
+
+    protected function deleteProductConfiguratorSettings(string $parentUuid, Context $context): void
+    {
+        $criteria = new Criteria();
+        $criteria->addAssociations(['configuratorSettings']);
+        $criteria->addFilter(new EqualsFilter('productId', $parentUuid));
+        $productConfiguratorSettings = $this->productConfiguratorRepository->searchIds($criteria, $context)->getIds();
+        $toDelete = [];
+        foreach ($productConfiguratorSettings as $productConfiguratorSetting) {
+            $primaryKey = [];
+            $primaryKey['id'] = $productConfiguratorSetting;
+            $toDelete[] = $primaryKey;
+        }
+        $this->productConfiguratorRepository->delete($toDelete, $context);
+    }
+
+    protected function deleteProductOptions(string $variantUuid, Context $context): void
+    {
+        $criteria = new Criteria();
+        $criteria->addAssociations(['configuratorSettings']);
+        $criteria->addFilter(new EqualsFilter('productId', $variantUuid));
+        $productOptions = $this->productOptionRepository->searchIds($criteria, $context)->getIds();
+        $this->productOptionRepository->delete($productOptions, $context);
     }
 }
